7ad99c56fc7872e733105cbdc4a1cfd457d3076c
Danielle Schugars <danielle@render.com>
2021-02-03 11:33:22 -0800

    arbitrary change

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(process.env["DB_CONNECTION_STRING"])
 
 sequelize.authenticate()
 .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log('Connection has been established successfully');
 })
 .catch(err => {
     console.error('Unable to connect to the database:', err);
